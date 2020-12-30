80a877f14076f71a6c5ac2f7cab676254aff837f
dineshgyawali <uttkrista@gmail.com>
2020-12-29 21:23:17 -0600

    Question 10 & 11 added

--- Question11.py
+++ Question11.py
@@ -6,11 +6,11 @@ while counter<5:
     number=int(input("Guess a lucky number "))
     
     if number==220:
-        print("Good Guess")
-        continue   
+        print("Good Guess!")
+        break  
             
     elif number!=220 and counter<5:
         print("Try Again")
 
 if counter==5:
-    print("Game Over!")
+    print("Sorry, but that was not very successful")
